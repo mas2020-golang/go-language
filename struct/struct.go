0907e2684117dec09333540a344b1dbd83b96144
Andrea Genovesi <andrea.genovesi@gmail.com>
2020-10-30 08:05:37 +0100

    changes to struct

--- struct/struct.go
+++ struct/struct.go
@@ -26,6 +26,9 @@ func (v Vehicle) Go() {
 func main() {
 	fmt.Println("---Init struct")
 	initStruct()
+	fmt.Println("---Compare struct")
+	compare()
+	fmt.Println("---Struct composition")
 
 	// use the composition
 	createSpecificType()
@@ -57,12 +60,10 @@ func initStruct() {
 	// with the zero value of the specific type
 	fmt.Println(new(Vehicle))
 	// It is similar to
-	fmt.Println(&Vehicle{})
-}
-
+	fmt.Printf("%#v\n",&Vehicle{})}
 /*
 Shows how to use pointers with struct.
- */
+*/
 func accessByPointers(){
 	var car *Vehicle = &Vehicle{brand: "Fiat",
 		model:      "Punto",
@@ -72,6 +73,25 @@ func accessByPointers(){
 	(*car).horsepower = 100
 	// or, Go dereferences the value automatically
 	car.horsepower = 100
+}
 
+/*
+Compare two structs together.
+*/
+func compare() {
+	v1 := Vehicle{
+		brand:      "Tesla",
+		model:      "V1",
+		horsepower: 125,
+		year:       2018,
+	}
 
+	v2 := Vehicle{
+		brand:      "Tesla",
+		model:      "V2",
+		horsepower: 135,
+		year:       2019,
+	}
+
+	fmt.Printf("v1 == v2 => %t\n", v1 == v2)
 }
